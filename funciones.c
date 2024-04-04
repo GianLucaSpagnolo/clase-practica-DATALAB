52fba1b94b5ce8cdf344c81e8654f10f156fc2c6
GianLucaSpagnolo <gianluuba@gmail.com>
2024-04-04 15:40:31 -0300

    function done

--- funciones.c
+++ funciones.c
@@ -3,16 +3,26 @@
 /**
  * Esta funcion va a leer las lineas de un archivo, y devuelve un vector de strings.
 */
-char** read_from_file(char* filename) {
-    
-    return 0;
+char **read_from_file(char *filename)
+{
+	return 0;
 }
-
 
 /**
  * Esta funcion recibe el vector de strings, y lo escribe en consola.
 */
-void write_from_file(char** file_data) {
+void write_from_file(char **file_data)
+{
+	if (file_data == NULL) {
+		printf("Error: No se pudo leer el archivo.\n");
+		return;
+	}
 
-    return;
+	for (int i = 0; i < 5; i++) {
+		printf("%s", file_data[i]);
+        free(file_data[i]);
+	}
+    free(file_data);
+
+	return;
 }
--- funciones.h
+++ funciones.h
@@ -2,6 +2,8 @@
 #define _FUNCS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char** read_from_file(char* filename);
 void write_from_file(char** file_data);
